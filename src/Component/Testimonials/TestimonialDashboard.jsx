7c6851b018ff73f737af25aca72b30c4b41a3886
Md-Mudassir <mdmudassirpro@gmail.com>
2021-03-03 22:43:22 +0530

    testimonial dashboard with form setup

--- src/Component/Testimonials/TestimonialDashboard.jsx
+++ src/Component/Testimonials/TestimonialDashboard.jsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import { makeStyles, CssBaseline, createMuiTheme, ThemeProvider } from '@material-ui/core';
 
-const TestimonialDashboard = () => {
-  return <div>hello</div>;
-};
+import Employees from './pages/Testimonials';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#333996',
+      light: '#3c44b126',
+    },
+    secondary: {
+      main: '#f83245',
+      light: '#f8324526',
+    },
+    background: {
+      default: '#f4f5fd',
+    },
+  },
+  props: {
+    MuiIconButton: {
+      disableRipple: true,
+    },
+  },
+});
+
+function TestimonialDashboard() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Employees />
+    </ThemeProvider>
+  );
+}
 
 export default TestimonialDashboard;
