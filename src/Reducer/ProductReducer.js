25296d3ed6316557721905d12a724a89f37ce198
selvakumar-icanio <selvakumar.s@icanio.com>
2021-05-29 18:42:40 +0530

    Product allocation added

--- src/Reducer/ProductReducer.js
+++ src/Reducer/ProductReducer.js
@@ -1,47 +1,59 @@
-import {PRODUCT} from '../Redux/Action'
+import { PRODUCT } from "../Redux/Action";
 const initialState = {
-    viewProductList : [],
-    addProduct:[],
-    editProduct : [],
-    deleteProduct : [],
-    addProductToStudent : [],
-    viewProductToStudentList : [],
-}
+  viewProductList: [],
+  addProduct: [],
+  editProduct: [],
+  deleteProduct: [],
+  addProductToStudentResponse: [],
+  studentProductList: [],
+  productFamilyList: [],
+  productVariantList : []
+};
 
 export default (state = initialState, action) => {
-	switch (action.type) {
-        case PRODUCT.viewProduct:
-			return {
-				...state,
-                viewProductList:action.viewProductList,
-            }
-		case PRODUCT.addProduct:
-			return {
-				...state,
-                addProduct:action.addProduct,
-            }
-            case PRODUCT.editProduct:
-                return {
-                    ...state,
-                    editProduct : action.editProduct
-                }
-                case PRODUCT.deleteProduct:
-                    return {
-                        ...state,
-                        deleteProduct : action.deleteProduct
-                    }
-                    case PRODUCT.addProductToStudent:
-                        return {
-                            ...state,
-                            addProductToStudent : action.addProductToStudent
-                        }
-                        case PRODUCT.viewProductToStudent:
-                            return {
-                                ...state,
-                                viewProductToStudentList : action.viewProductToStudentList
-                            }
-		default:
-			break
-	}
-	return state
-}
+  switch (action.type) {
+    case PRODUCT.viewProduct:
+      return {
+        ...state,
+        viewProductList: action.viewProductList,
+      };
+    case PRODUCT.addProduct:
+      return {
+        ...state,
+        addProduct: action.addProduct,
+      };
+    case PRODUCT.editProduct:
+      return {
+        ...state,
+        editProduct: action.editProduct,
+      };
+    case PRODUCT.deleteProduct:
+      return {
+        ...state,
+        deleteProduct: action.deleteProduct,
+      };
+    case PRODUCT.addProductToStudent:
+      return {
+        ...state,
+        addProductToStudentResponse: action.payload,
+      };
+    case PRODUCT.viewProductToStudent:
+      return {
+        ...state,
+        studentProductList: action.payload,
+      };
+      case PRODUCT.getAllProductFamily:
+        return {
+          ...state,
+          productFamilyList: action.payload,
+        };
+        case PRODUCT.getProductByFamilyId:
+          return {
+            ...state,
+            productVariantList: action.payload,
+          };
+    default:
+      break;
+  }
+  return state;
+};
