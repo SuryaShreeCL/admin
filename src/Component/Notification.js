5b3a5f9ee285e2bacc794c554acc23a7a66974d2
venkatesh-icanio <venkatesh.m@icanio.com>
2021-06-05 17:53:09 +0530

    updatednotification

--- src/Component/Notification.js
+++ src/Component/Notification.js
@@ -18,6 +18,8 @@ import CloseIcon from "@material-ui/icons/Close";
 import {connect} from 'react-redux'
 import {viewnotification,addnotification,updatenotification,deletenotification} from '../Actions/Notification'
 import TableComponent from "./TableComponent/TableComponent";
+import MySnackBar from "./MySnackBar";
+import { isEmptyString } from './Validation';
 
 export class Notification extends Component {
 
@@ -39,6 +41,20 @@ export class Notification extends Component {
             tenMinutes : "",
             show : false,
             deleteshow: false,
+            snackMsg: "",
+            snackVariant: "",
+            snackOpen: false,
+            featureNameErr:"",
+            codeErr:"",
+            completedTaskErr:"",
+            firstDayErr:"",
+            secondDayErr:"",
+            thirdDayErr:"",
+            fourthDayErr:"",
+            fifthDayErr:"",
+            sixthDayErr:"",
+            seventhDayErr:"",
+            tenMinutesErr:""
         }
     }
 
@@ -213,15 +229,99 @@ spinnerTheme = () =>createMuiTheme({
       // alert("confirm")      
     this.props.deletenotification(this.state.notificationId);
     this.setState({
-      deleteshow:false
+      deleteshow:false,
+      snackMsg: "Deleted Suceessfully ",
+      snackVariant: "success",
+      snackOpen: true,
     })
     }
 
     // Add Notification
     newNotify = () =>{
-      this.setState({ show: false });
-      let obj = {
-        code: parseInt(this.state.code),
+      let helperTxt = "Please fill the Required Field";
+      isEmptyString(this.state.code) ? this.setState({codeErr:helperTxt}) : this.setState({codeErr:""})
+      isEmptyString(this.state.featureName) ? this.setState({featureNameErr:helperTxt}) : this.setState({featureNameErr:""})
+      isEmptyString(this.state.completedTask) ? this.setState({completedTaskErr:helperTxt}) : this.setState({completedTaskErr:""})
+      isEmptyString(this.state.firstDay) ? this.setState({firstDayErr:helperTxt}) : this.setState({firstDayErr:""})
+      isEmptyString(this.state.secondDay) ? this.setState({secondDayErr:helperTxt}) : this.setState({secondDayErr:""})
+      isEmptyString(this.state.thirdDay) ? this.setState({thirdDayErr:helperTxt}) : this.setState({thirdDayErr:""})
+      isEmptyString(this.state.fourthDay) ? this.setState({fourthDayErr:helperTxt}) : this.setState({fourthDayErr:""})
+      isEmptyString(this.state.fifthDay) ? this.setState({fifthDayErr:helperTxt}) : this.setState({fifthDayErr:""})
+      isEmptyString(this.state.sixthDay) ? this.setState({sixthDayErr:helperTxt}) : this.setState({sixthDayErr:""})
+      isEmptyString(this.state.seventhDay) ? this.setState({seventhDayErr:helperTxt}) : this.setState({seventhDayErr:""})
+      isEmptyString(this.state.tenMinutes) ? this.setState({tenMinutesErr:helperTxt}) : this.setState({tenMinutesErr:""})
+      {
+        // this.setState({ show: false });
+        let obj = {
+          code: parseInt(this.state.code),
+          featureName: this.state.featureName,
+          completedTask : this.state.completedTask,
+          firstDay : this.state.firstDay,
+          secondDay : this.state.secondDay,
+          thirdDay : this.state.thirdDay,
+          fourthDay : this.state.fourthDay,
+          fifthDay : this.state.fifthDay,
+          sixthDay : this.state.sixthDay,
+          seventhDay : this.state.seventhDay,
+          tenMinutes : this.state.tenMinutes,
+  
+        };
+        if (this.state.id.length === 0 &&
+          !isEmptyString(this.state.code) &&
+          !isEmptyString(this.state.featureName) &&
+          !isEmptyString(this.state.completedTask) &&
+          !isEmptyString(this.state.firstDay) &&
+          !isEmptyString(this.state.secondDay) &&
+          !isEmptyString(this.state.thirdDay) &&
+          !isEmptyString(this.state.fourthDay) &&
+          !isEmptyString(this.state.fifthDay) &&
+          !isEmptyString(this.state.sixthDay) &&
+          !isEmptyString(this.state.seventhDay) &&
+          !isEmptyString(this.state.tenMinutes) 
+          ) {
+          this.props.addnotification(obj);
+          this.setState({
+            id: "",
+            code:"",
+            featureName : "",
+            completedTask: "",
+            firstDay : "",
+            secondDay : "",
+            thirdDay : "",
+            fourthDay : "",
+            fifthDay : "",
+            sixthDay : "",
+            seventhDay : "",
+            tenMinutes : "",
+            snackMsg: "Added Suceessfully ",
+            snackVariant: "success",
+            snackOpen: true,
+          });
+        }
+      } 
+      // this.props.getPaginateNotification(0, 20,null);    
+      }
+
+  // Update Notification
+  updateNotify = () =>{
+    let helperTxt = "Please fill the Required Field";
+    isEmptyString(this.state.code) ? this.setState({codeErr:helperTxt}) : this.setState({codeErr:""})
+    isEmptyString(this.state.featureName) ? this.setState({featureNameErr:helperTxt}) : this.setState({featureNameErr:""})
+    isEmptyString(this.state.completedTask) ? this.setState({completedTaskErr:helperTxt}) : this.setState({completedTaskErr:""})
+    isEmptyString(this.state.firstDay) ? this.setState({firstDayErr:helperTxt}) : this.setState({firstDayErr:""})
+    isEmptyString(this.state.secondDay) ? this.setState({secondDayErr:helperTxt}) : this.setState({secondDayErr:""})
+    isEmptyString(this.state.thirdDay) ? this.setState({thirdDayErr:helperTxt}) : this.setState({thirdDayErr:""})
+    isEmptyString(this.state.fourthDay) ? this.setState({fourthDayErr:helperTxt}) : this.setState({fourthDayErr:""})
+    isEmptyString(this.state.fifthDay) ? this.setState({fifthDayErr:helperTxt}) : this.setState({fifthDayErr:""})
+    isEmptyString(this.state.sixthDay) ? this.setState({sixthDayErr:helperTxt}) : this.setState({sixthDayErr:""})
+    isEmptyString(this.state.seventhDay) ? this.setState({seventhDayErr:helperTxt}) : this.setState({seventhDayErr:""})
+    isEmptyString(this.state.tenMinutes) ? this.setState({tenMinutesErr:helperTxt}) : this.setState({tenMinutesErr:""})
+    {
+      // this.setState({ show: false });
+      let 
+      obj1 = {
+        id : this.state.id,
+        code:this.state.code,
         featureName: this.state.featureName,
         completedTask : this.state.completedTask,
         firstDay : this.state.firstDay,
@@ -229,67 +329,45 @@ spinnerTheme = () =>createMuiTheme({
         thirdDay : this.state.thirdDay,
         fourthDay : this.state.fourthDay,
         fifthDay : this.state.fifthDay,
-        sixthDay : this.state.sixthDay,
+        sixthDay :this.state.sixthDay,
         seventhDay : this.state.seventhDay,
-        tenMinutes : this.state.tenMinutes,
-
+        tenMinutes : this.state.tenMinutes
       };
-      if (this.state.id.length === 0) {
-        this.props.addnotification(obj);
+      if (this.state.id.length !== 0 &&
+          !isEmptyString(this.state.code) &&
+          !isEmptyString(this.state.featureName) &&
+          !isEmptyString(this.state.completedTask) &&
+          !isEmptyString(this.state.firstDay) &&
+          !isEmptyString(this.state.secondDay) &&
+          !isEmptyString(this.state.thirdDay) &&
+          !isEmptyString(this.state.fourthDay) &&
+          !isEmptyString(this.state.fifthDay) &&
+          !isEmptyString(this.state.sixthDay) &&
+          !isEmptyString(this.state.seventhDay) &&
+          !isEmptyString(this.state.tenMinutes) 
+        ) {
+        this.props.updatenotification(this.state.id,obj1);
         this.setState({
           id: "",
           code:"",
-          featureName : "",
-          completedTask: "",
+          featureName: "",
+          completedTask : "",
           firstDay : "",
           secondDay : "",
           thirdDay : "",
           fourthDay : "",
-          fifthDay : "",
-          sixthDay : "",
-          seventhDay : "",
-          tenMinutes : ""
-        });
+          fifthDay:"",
+          sixthDay:"",
+          seventhDay:"",
+          tenMinutes:"",
+          update: true,
+          snackMsg: "Updated Suceessfully ",
+          snackVariant: "success",
+          snackOpen: true,
+        });      
       }
-      // this.props.getPaginateNotification(0, 20,null);    
-      }
-
-  // Update Notification
-  updateNotify = () =>{
-      this.setState({ show: false });
-  let 
-  obj1 = {
-    id : this.state.id,
-    code:this.state.code,
-    featureName: this.state.featureName,
-    completedTask : this.state.completedTask,
-    firstDay : this.state.firstDay,
-    secondDay : this.state.secondDay,
-    thirdDay : this.state.thirdDay,
-    fourthDay : this.state.fourthDay,
-    fifthDay : this.state.fifthDay,
-    sixthDay :this.state.sixthDay,
-    seventhDay : this.state.seventhDay,
-    tenMinutes : this.state.tenMinutes
-  };
-  if (this.state.id.length !== 0) {
-    this.props.updatenotification(this.state.id,obj1);
-    this.setState({
-      id: "",
-      code:"",
-      featureName: "",
-      completedTask : "",
-      firstDay : "",
-      secondDay : "",
-      thirdDay : "",
-      fourthDay : "",
-      fifthDay:"",
-      sixthDay:"",
-      seventhDay:"",
-      tenMinutes:"",
-      update: true,
-    });      
-  }
+    }
+    
   // this.props.getPaginateNotification(0, 20,null);    
 }
  column = [
@@ -377,6 +455,8 @@ spinnerTheme = () =>createMuiTheme({
                       color="primary"
                       label="Featurename"
                       fullWidth
+                      error={this.state.featureNameErr.length > 0}
+                      helperText={this.state.featureNameErr}
                       value={this.state.featureName}
                       onChange={(e) => this.setState({ featureName: e.target.value })}
                       multiline
@@ -388,6 +468,8 @@ spinnerTheme = () =>createMuiTheme({
                       color="primary"
                       label="Code"
                       fullWidth
+                      error={this.state.codeErr.length > 0}
+                      helperText={this.state.codeErr}
                       value={this.state.code}
                       onChange={(e) => this.setState({ code: e.target.value })}
                     />
@@ -398,6 +480,8 @@ spinnerTheme = () =>createMuiTheme({
                       color="primary"
                       label="Completed Task"
                       fullWidth
+                      error={this.state.completedTaskErr.length > 0}
+                      helperText={this.state.completedTaskErr}
                       value={this.state.completedTask}
                       onChange={(e) => this.setState({ completedTask: e.target.value })}
                       multiline
@@ -411,6 +495,8 @@ spinnerTheme = () =>createMuiTheme({
                       rowsMin={3}
                       multiline
                       fullWidth
+                      error={this.state.firstDayErr.length > 0}
+                      helperText={this.state.firstDayErr}
                       value={this.state.firstDay}
                       onChange={(e) =>
                         this.setState({ firstDay: e.target.value })
@@ -423,6 +509,8 @@ spinnerTheme = () =>createMuiTheme({
                       color="primary"
                       label="SecondDay"
                       fullWidth
+                      error={this.state.secondDayErr.length > 0}
+                      helperText={this.state.secondDayErr}
                       value={this.state.secondDay}
                       onChange={(e) => this.setState({ secondDay: e.target.value })}
                       multiline
@@ -434,6 +522,8 @@ spinnerTheme = () =>createMuiTheme({
                       color="primary"
                       label="ThirdDay"
                       fullWidth
+                      error={this.state.thirdDayErr.length > 0}
+                      helperText={this.state.thirdDayErr}
                       value={this.state.thirdDay}
                       onChange={(e) => this.setState({ thirdDay: e.target.value })}
                       multiline
@@ -445,6 +535,8 @@ spinnerTheme = () =>createMuiTheme({
                       color="primary"
                       label="FourthDay"
                       fullWidth
+                      error={this.state.fourthDayErr.length > 0}
+                      helperText={this.state.fourthDayErr}
                       value={this.state.fourthDay}
                       onChange={(e) => this.setState({ fourthDay: e.target.value })}
                       multiline
@@ -456,6 +548,8 @@ spinnerTheme = () =>createMuiTheme({
                       color="primary"
                       label="FifthDay"
                       fullWidth
+                      error={this.state.fifthDayErr.length > 0}
+                      helperText={this.state.fifthDayErr}
                       value={this.state.fifthDay}
                       onChange={(e) => this.setState({ fifthDay: e.target.value })}
                       multiline
@@ -467,6 +561,8 @@ spinnerTheme = () =>createMuiTheme({
                       color="primary"
                       label="SixthDay"
                       fullWidth
+                      error={this.state.sixthDayErr.length > 0}
+                      helperText={this.state.sixthDayErr}
                       value={this.state.sixthDay}
                       onChange={(e) => this.setState({ sixthDay: e.target.value })}
                       multiline
@@ -478,6 +574,8 @@ spinnerTheme = () =>createMuiTheme({
                       color="primary"
                       label="SeventhDay"
                       fullWidth
+                      error={this.state.seventhDayErr.length > 0}
+                      helperText={this.state.seventhDayErr}
                       value={this.state.seventhDay}
                       onChange={(e) => this.setState({ seventhDay: e.target.value })}
                       multiline
@@ -488,6 +586,8 @@ spinnerTheme = () =>createMuiTheme({
                       variant="outlined"
                       color="primary"
                       label="TenMinutes"
+                      error={this.state.tenMinutesErr.length > 0}
+                      helperText={this.state.tenMinutesErr}
                       fullWidth
                       value={this.state.tenMinutes}
                       onChange={(e) => this.setState({ tenMinutes: e.target.value })}
@@ -531,6 +631,12 @@ spinnerTheme = () =>createMuiTheme({
                   </DialogContent>
                 </Dialog>
               </ThemeProvider>
+              <MySnackBar
+              snackMsg={this.state.snackMsg}
+              snackVariant={this.state.snackVariant}
+              snackOpen={this.state.snackOpen}
+              onClose={() => this.setState({ snackOpen: false })}
+            />
           </ThemeProvider>
          
         )
