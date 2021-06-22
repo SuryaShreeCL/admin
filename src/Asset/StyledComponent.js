31e739297f9def57609d815da9f788f1c9c6946d
Sankaraveni-icanio <sankaraveni.m@icanio.com>
2021-06-22 18:23:21 +0530

    product punching route

--- src/Asset/StyledComponent.js
+++ src/Asset/StyledComponent.js
@@ -1,20 +1,20 @@
 import styled from "styled-components";
 
 export const UpdateDetailButton = styled.div`
-position: absolute;
-// left: 44.03%;
-// right: 44.03%;
-// top: 89.67%;
-// bottom: 6.89%;
+  position: absolute;
+  // left: 44.03%;
+  // right: 44.03%;
+  // top: 89.67%;
+  // bottom: 6.89%;
 
-/* primery */
+  /* primery */
 
-background: #1093FF;
-border-radius: 30px;
-outline : none;
-// width : 20%;
-height: 5%;
-// display : flex;
-// align-items : center;
-// justify-content : center;
+  background: #1093ff;
+  border-radius: 30px;
+  outline: none;
+  // width : 20%;
+  height: 5%;
+  // display : flex;
+  // align-items : center;
+  // justify-content : center;
 `;
