9ca7734e7909aeb2a0026d55ca7fe98bd086073c
Md-Mudassir47 <mohammed.m@thecareerlabs.com>
2021-06-29 13:45:04 +0530

    dynamic images

--- src/Component/Wall/Components/Preview.jsx
+++ src/Component/Wall/Components/Preview.jsx
@@ -4,6 +4,21 @@ import { PreviewContainer, Frame, Post } from '../Assets/Styles/PreviewStyles';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import ShareIcon from '@material-ui/icons/Share';
 import CommentIcon from '@material-ui/icons/Comment';
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+
+const Images = [
+  {
+    src:
+      'https://previews.123rf.com/images/kho/kho1406/kho140600092/29092622-beautiful-girl-reading-book-in-the-summer-park-image-toned-.jpg',
+  },
+  {
+    src: 'https://image.freepik.com/free-photo/cute-young-lady-reading-book_23-2148204301.jpg',
+  },
+  {
+    src: 'https://media.istockphoto.com/photos/beautiful-lady-reading-a-book-picture-id183825490',
+  },
+];
 
 const Preview = () => {
   return (
@@ -12,10 +27,11 @@ const Preview = () => {
         <img src={iPhoneFrame} alt='iPhone 8 Frame' />
         <Post>
           <div className='Poster'>
-            <img
-              src='https://previews.123rf.com/images/kho/kho1406/kho140600092/29092622-beautiful-girl-reading-book-in-the-summer-park-image-toned-.jpg'
-              alt=''
-            />
+            <Carousel showArrows={true} infiniteLoop={true} autoPlay={true} showThumbs={false}>
+              {Images.map((image) => {
+                return <img style={{ maxHeight: '300px' }} src={image.src} />;
+              })}
+            </Carousel>
           </div>
           <div className='CaptionContainer'>
             <h5>#Science</h5>
