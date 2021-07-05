72908fd3c3a340eebcaf3cd7bd79bd212f2241e4
selvakumar-icanio <selvakumar.s@icanio.com>
2021-07-05 12:23:08 +0530

    Call summary

---
+++ src/Component/ObCallSummary/CallSummaryLayout.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CallSummaryLayout extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default CallSummaryLayout;
