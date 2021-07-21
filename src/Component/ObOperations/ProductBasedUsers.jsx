18d014a4e1c1c8f9e3d6c98fca7c7ffc9174677a
selvakumar-icanio <selvakumar.s@icanio.com>
2021-07-21 08:40:35 +0530

    Changes

--- src/Component/ObOperations/ProductBasedUsers.jsx
+++ src/Component/ObOperations/ProductBasedUsers.jsx
@@ -3,12 +3,14 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Onboarding from '../ObOnboarding/Onboarding';
 import { ThemedTab, ThemedTabs } from '../Utils/ThemedComponents';
-
+import { getAdminLinkedProduct } from "../../Actions/AdminAction"
 class ProductBasedUsers extends Component {
     constructor(props){
         super(props);
         this.state = {
-            tabCount : 0
+            tabCount : 0,
+            selectedItem : null,
+            productDetails : null,
         }
     }
 
@@ -22,25 +24,58 @@ class ProductBasedUsers extends Component {
         }
     }
 
+    componentDidMount() {
+        this.props.getAdminLinkedProduct()
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if(this.props.adminLinkedProductDetails !== prevProps.adminLinkedProductDetails){
+            var sortedArr =  this.props.adminLinkedProductDetails.products.length > 0 && this.props.adminLinkedProductDetails.products[0].steps.sort((a,b) => a.rank-b.rank)
+            console.log(sortedArr)
+            sortedArr !== false && sortedArr.map((it,ix)=>{
+                it.steps.sort((c,d)=>c.rank - d.rank)
+            })
+            console.log(sortedArr)
+            this.setState({
+                productDetails : sortedArr,
+            })
+        }
+    }
+    
+    
+
     render() {
+        console.log(this.state)
+        var componentList = {
+            "Onboarding" : "Onboarding",
+        }
+        console.log(this.state.selectedItem !== null && this.state.selectedItem.stepName)
+        var obj = {
+            Onboarding : Onboarding,
+          
+        }
+        var selectedComponent = this.state.selectedItem !== null && componentList.[this.state.selectedItem.stepName]
+        var Page = obj[selectedComponent];
         return (
            <Grid container>
                <Grid item md={12}>
                <ThemedTabs
-             value={this.state.tabCount}
+             value={this.state.selectedItem}
              textColor={"inherit"}
-             onChange={(e, value) => this.setState({ tabCount: value })}
+             onChange={(e, value) => this.setState({selectedItem : value})}
              aria-label="ant example"
            >
-             <ThemedTab label="Onboarding" />
-             <ThemedTab label="Profile Gap Analysis" />
-             <ThemedTab label="Strategy Session" />
-             <ThemedTab label="Application Stage" />
-             <ThemedTab label="Post Admit Services" />
+            
+               {this.state.productDetails !== null && this.state.productDetails.map((item,index)=>{
+                   return (
+                    <ThemedTab value={item} label={item.stepName} />
+                   )    
+               })}
+         
            </ThemedTabs>
                </Grid>
                <Grid item md={12}>
-                {this.renderContent(this.state.tabCount)}
+               {Page !== undefined && <Page {...this.props} />  }             
                </Grid>
            </Grid>
         );
@@ -48,11 +83,11 @@ class ProductBasedUsers extends Component {
 }
 
  const mapStateToProps = (state) =>({
-    
+    adminLinkedProductDetails : state.AdminReducer.adminLinkedProductDetails
 })
 
 const useStyles = () =>({
 
 })
 
-export default connect(mapStateToProps,{})(withStyles(useStyles)(ProductBasedUsers))
+export default connect(mapStateToProps,{ getAdminLinkedProduct })(withStyles(useStyles)(ProductBasedUsers))
