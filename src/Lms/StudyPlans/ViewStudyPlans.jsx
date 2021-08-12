d185f117df4ee05791c3024deaed114df5720f28
Sankaraveni-icanio <sankaraveni.m@icanio.com>
2021-08-12 15:40:14 +0530

    check

---
+++ src/Lms/StudyPlans/ViewStudyPlans.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ViewStudyPlans extends Component {
+    render() {
+        return (
+            <div>
+                <p>hello</p>
+            </div>
+        )
+    }
+}
