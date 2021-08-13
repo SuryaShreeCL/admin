9cb6ecf9ef1e8ddca3edf44ad2728b1307277bc2
Balasuresh-icanio <balasuresh.p@icanio.com>
2021-08-13 18:01:54 +0530

    new task

--- src/Lms/Component/CourseMaterials/addNewTopic/Index.jsx
+++ src/Lms/Component/CourseMaterials/addNewTopic/Index.jsx
@@ -1,5 +1,20 @@
 import React, { Component } from "react";
 import TinyEditor from "../../../Utils/textEditor/TinyEditor";
+import { Box } from "@material-ui/core";
+import Preview from "../../../Assets/icons/preview.svg";
+import {
+  ButtonContainer,
+  Container,
+  FillButton,
+  InputCard,
+  MainContainer,
+  OutlineButton,
+  PreviewIcon,
+  TabContainer,
+  TabItem,
+  Title,
+  Wrapper,
+} from "../../../Assets/StyledComponents";
 
 class Index extends Component {
   constructor(props) {
@@ -18,8 +33,32 @@ class Index extends Component {
   render() {
     return (
       <>
-        <TinyEditor onEditorChange={this.onEditorTextChange} />
-        <p>{this.state.editorSource}</p>
+        <MainContainer>
+          <Container>
+            <Wrapper>
+              <Title>Add New Topic</Title>
+              <InputCard></InputCard>
+              <TabContainer>
+                <TabItem key={0} className={"active__task__tab"}>
+                  Task 1
+                </TabItem>
+                <TabItem key={1}>Task 2</TabItem>
+              </TabContainer>
+              <InputCard></InputCard>
+              <div style={{ padding: "8px" }}>
+                <TinyEditor onEditorChange={this.onEditorTextChange} />
+              </div>
+              <p>{this.state.editorSource}</p>
+            </Wrapper>
+          </Container>
+          <ButtonContainer>
+            <OutlineButton>
+              <PreviewIcon src={Preview} /> Preview
+            </OutlineButton>
+            <OutlineButton>Cancel</OutlineButton>
+            <FillButton>Save</FillButton>
+          </ButtonContainer>
+        </MainContainer>
       </>
     );
   }
