29c6cd5db5e36a8063763d8e116afff05fc8f279
joe-icanio <joerobin.a@gmail.com>
2021-08-14 12:04:30 +0530

    Drop Down Component

--- src/Lms/Utils/DropDown.jsx
+++ src/Lms/Utils/DropDown.jsx
@@ -6,16 +6,82 @@ import {
   Select,
   ThemeProvider,
   MenuItem,
+  withStyles,
 } from '@material-ui/core';
+
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+
+const DropDownIcon = withStyles({
+  root:{
+    fill:'#323232',
+  }
+})(ArrowDropDownIcon);
+// export const ButtonIcon = withStyles({
+//   root: {
+//     padding: '8px',
+//     borderRadius: '4px',
+//     // background: '#aqua',
+//   },
+// })(IconButton);
 
 const selectTheme = createTheme({
   overrides: {
     MuiSelect: {
       select: {
-        minWidth: '250px',
-        maxWidth: '350px',
+        width:'350px',
+        // minWidth: '350px',
+        // maxWidth: '350px',
+        '&:focus':{
+          backgroundColor:'rgba(5, 42, 78, 0.05)'
+        },
+        fontStyle: "normal", fontWeight: "normal", fontSize: "16px", lineHeight: "20px", color: "#052A4E",
       },
     },
+    MuiInputLabel:{
+      shrink:{
+        transform:'translate(14px, -6px) scale(0.75)',
+      },
+      formControl:{
+        // top:'20px',
+        // left:'12px',
+        transform:'translate(8px, 22px) scale(1)',
+      }
+      
+    },
+    MuiListItem:{
+      root:{
+        fontStyle: "normal", fontWeight: "normal", fontSize: "16px", lineHeight: "20px", color: "#052A4E",
+      }
+    }
+   
+    
+    // MuiListItem:{
+    //   button:{
+    //     '&:hover':{
+    //       backgroundColor:'rgba(16, 147, 255, 0.4)'
+    //       // rgba(, 1)
+
+
+    //     }
+    //   },
+    //   // root:{
+    //   //   selected:{
+    //   //     backgroundColor:'blue',
+    //   //     // backgroundColor:'rgba(16, 147, 255, 0.8)'
+    //   //   },
+    //   // },
+    //   root:{
+    //     backgroundColor:'blue',
+    
+    //     // '& .Mui-selected':{
+    //     //   backgroundColor:'rgba(5, 42, 78, 0.8)'
+    //     //   // '&:hover':{
+            
+
+    //     //   // }
+    //     // }
+    //   }
+    // }
     // MuiInputLabel: {
     //   FormControl: {
     //     // top: '-6px',
@@ -38,7 +104,7 @@ export default function DropDown(props) {
         <FormControl>
           <InputLabel id={label}>{label}</InputLabel>
 
-          <Select fullWidth variant='outlined' labelId={label} label={label}>
+          <Select fullWidth variant='outlined' labelId={label} label={label} id={props.label} IconComponent={DropDownIcon}>
             {props.items !== undefined &&
               props.items.map(item => (
                 <MenuItem value={item.value}>{item.label}</MenuItem>
