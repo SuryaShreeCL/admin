5adcd36f88c103e591187f2df83898afe6463404
Sankaraveni-icanio <sankaraveni.m@icanio.com>
2021-08-17 09:10:53 +0530

    check

--- src/Lms/Index.jsx
+++ src/Lms/Index.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import ViewStudyPlans from './StudyPlans/ViewStudyPlans'
 
 export default class Index extends Component {
     render() {
         return (
             <div>
-                
+                <ViewStudyPlans/>
             </div>
         )
     }
