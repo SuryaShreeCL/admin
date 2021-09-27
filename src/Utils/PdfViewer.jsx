25c339d2e4d92e9b19310a11d83f0f874ccf72ca
selvakumar-icanio <selvakumar.s@icanio.com>
2021-09-27 19:38:21 +0530

    Fixes

--- src/Utils/PdfViewer.jsx
+++ src/Utils/PdfViewer.jsx
@@ -9,9 +9,29 @@ import {
 import "@react-pdf-viewer/core/lib/styles/index.css";
 import "@react-pdf-viewer/selection-mode/lib/styles/index.css";
 import "@react-pdf-viewer/zoom/lib/styles/index.css";
-
 import SamplePdf from "../Asset/Images/samplePdf.pdf";
+import { makeStyles } from "@material-ui/core";
 function PdfViewer(props) {
+  const useStyles = makeStyles((theme)=>({
+    pdfWrapper : {
+      border: "1px solid rgba(0, 0, 0, 0.3)",
+      display: "flex",
+      flexDirection: "column",
+      height: "82vh",
+      [theme.breakpoints.down("lg")] : {
+        height: "106vh",
+      }
+    },
+    pdfViewer : {
+      alignItems: "center",
+      backgroundColor: "#eeeeee",
+      borderBottom: "1px solid rgba(0, 0, 0, 0.1)",
+      display: "flex",
+      justifyContent: "center",
+      padding: "4px",
+    }
+  }))
+  const classes = useStyles()
   const zoomPluginInstance = zoomPlugin();
   const selectionModePluginInstance = selectionModePlugin();
   const { SwitchSelectionModeButton } = selectionModePluginInstance;
@@ -20,23 +40,10 @@ function PdfViewer(props) {
   return (
     <Worker workerUrl="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.6.347/pdf.worker.min.js">
       <div
-        className="rpv-core__viewer"
-        style={{
-          border: "1px solid rgba(0, 0, 0, 0.3)",
-          display: "flex",
-          flexDirection: "column",
-          height: "82vh",
-        }}
+        className={classes.pdfWrapper}
       >
         <div
-          style={{
-            alignItems: "center",
-            backgroundColor: "#eeeeee",
-            borderBottom: "1px solid rgba(0, 0, 0, 0.1)",
-            display: "flex",
-            justifyContent: "center",
-            padding: "4px",
-          }}
+          className={classes.pdfViewer}
         >
           <ZoomOutButton />
           <ZoomPopover />
