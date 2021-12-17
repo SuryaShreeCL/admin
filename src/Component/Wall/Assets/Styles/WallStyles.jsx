d813deee3a5931df77e347827c80fd75fcf2de59
Md-Mudassir47 <mohammed.m@thecareerlabs.com>
2021-12-17 23:41:15 +0530

    card update

--- src/Component/Wall/Assets/Styles/WallStyles.jsx
+++ src/Component/Wall/Assets/Styles/WallStyles.jsx
@@ -84,7 +84,7 @@ export const WebinarTabContainer = styled.section`
       box-shadow: 0px 0px 8px rgba(1, 20, 70, 0.1);
       border-radius: 8px;
       min-width: 30%;
-      max-width: 33%;
+      max-width: 40%;
       margin: 1rem 1.2rem 1rem 0;
 
       h6 {
