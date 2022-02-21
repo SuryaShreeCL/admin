7e6cb1067c1104a89a73cce0ca3da911f1f8adf3
Debarun Mitra <85116424+DebarunMitra1991@users.noreply.github.com>
2022-02-21 17:00:57 +0530

    post test upload status file functionality added

--- src/Actions/TestActions.js
+++ src/Actions/TestActions.js
@@ -240,6 +240,31 @@ export const getTestDetails = (id) => async (dispatch) => {
   }
 };
 
+export const uploadPostTestStatusByStepId = (sheet, stepId, callback) => {
+  return dispatch => {
+    axios
+      .post(
+        `${process.env.REACT_APP_API_URL}/api/v1/students/uploadStudentPostTestStepStatus/${stepId}`,
+        sheet,
+        {
+          crossDomain: true,
+          headers: {
+            admin: "yes",
+            Authorization: `Bearer ${window.sessionStorage.getItem(
+              "accessToken"
+            )}`,
+          },
+        }
+      )
+      .then(response => {
+        callback(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+};
+
 export const scheduleIt = (id) => {
   axios({
     method: 'put',
