6d53b42079b1fe6c95507b3f5871f378d45452c8
Md-Mudassir47 <mohammed.m@thecareerlabs.com>
2022-02-23 12:50:00 +0530

    comments removed

--- src/Component/Wall/Pages/EditPost.jsx
+++ src/Component/Wall/Pages/EditPost.jsx
@@ -747,7 +747,7 @@ const EditPost = () => {
                         </MuiPickersUtilsProvider>
                       )}
                     </Grid>
-                    <pre>{JSON.stringify({ values }, null, 4)}</pre>
+                    {/* <pre>{JSON.stringify({ values }, null, 4)}</pre> */}
                   </Form>
                   {values.isWebinar ? null : <Preview state={values} />}
                 </div>
