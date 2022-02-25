20972f9d1e5aa425fe6b9c83e6e05658f81ffe22
sankaraveni-icanio <sankaraveni.m@icanio.com>
2022-02-25 08:53:31 +0530

    dashboard bug fix

--- src/Component/ProfileGapAnalysis/Dashboard.js
+++ src/Component/ProfileGapAnalysis/Dashboard.js
@@ -294,7 +294,8 @@ class Dashboard extends Component {
                   );
                 })}
               {/* post pga card details */}
-              {/* {this.state.calldetails &&
+              {this.state.calldetails &&
+                this.state.calldetails.postPga &&
                 this.state.calldetails.postPga.map((data) => {
                   let date = new Date(data.datetime).getDate();
                   let month = new Date(data.datetime).getMonth() + 1;
@@ -371,7 +372,7 @@ class Dashboard extends Component {
                       </ThemeProvider>
                     </Grid>
                   );
-                })} */}
+                })}
             </Grid>
           </Grid>
         </Grid>
