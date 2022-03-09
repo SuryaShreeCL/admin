3d0c9c8d876b8bb40e7f5abd101bbe5ed1adfb17
Md-Mudassir47 <mohammed.m@thecareerlabs.com>
2022-03-09 20:14:53 +0530

    nullable enabled

--- src/Component/Wall/Pages/EditPost.jsx
+++ src/Component/Wall/Pages/EditPost.jsx
@@ -226,18 +226,21 @@ const EditPost = () => {
         heading: yup.string().required('Heading is Required'),
         subText: yup.string().required('Sub Heading is Required'),
         message: yup.string().required('Message is Required'),
-        form: yup.object().shape({
-          formQuestions: yup.array().of(
-            yup.object().shape({
-              questionText: yup.string().required('Question is Required'),
-              formQuestionsChoices: yup.array().of(
-                yup.object().shape({
-                  questionChoice: yup.string().required('Choice is required'),
-                })
-              ),
-            })
-          ),
-        }),
+        form: yup
+          .object()
+          .shape({
+            formQuestions: yup.array().of(
+              yup.object().shape({
+                questionText: yup.string().required('Question is Required'),
+                formQuestionsChoices: yup.array().of(
+                  yup.object().shape({
+                    questionChoice: yup.string().required('Choice is required'),
+                  })
+                ),
+              })
+            ),
+          })
+          .nullable(true),
       })
     ),
   });
@@ -871,7 +874,7 @@ const EditPost = () => {
                         </MuiPickersUtilsProvider>
                       )}
                     </Grid>
-                    {/* <pre>{JSON.stringify({ values }, null, 4)}</pre> */}
+                    {/* <pre>{JSON.stringify({ errors }, null, 4)}</pre> */}
                   </Form>
                   {/* Swetha */}
                   <div style={{ flexDirection: 'column' }}>
