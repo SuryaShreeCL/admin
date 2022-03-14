f5fd1e2deddfb5ebb09d41957f08d1c83c22fe6e
balasuresh-icanio <balasuresh.p@icanio.com>
2022-03-14 10:46:41 +0530

    login role dropdown added

--- src/Component/Login.js
+++ src/Component/Login.js
@@ -1,21 +1,23 @@
-import React, { Component } from 'react';
-import { TextField, Button } from '@material-ui/core';
-import '../Asset/Login.css';
-import GoogleBtn from './GoogleBtn';
-import { landingAdminPath, rootPath, studentPath } from './RoutePaths';
-import { connect } from 'react-redux';
-import history from './History';
-import Visibility from '@material-ui/icons/Visibility';
-import VisibilityOff from '@material-ui/icons/VisibilityOff';
-import IconButton from '@material-ui/core/IconButton';
-import InputAdornment from '@material-ui/core/InputAdornment';
-import OutlinedInput from '@material-ui/core/OutlinedInput';
-import InputLabel from '@material-ui/core/InputLabel';
-import FormHelperText from '@material-ui/core/FormHelperText';
-import FormControl from '@material-ui/core/FormControl';
-import { adminLogin } from '../Actions/AdminAction';
-import { isEmptyString } from './Validation';
-import MySnackBar from './MySnackBar';
+import React, { Component } from "react";
+import { TextField, Button } from "@material-ui/core";
+import "../Asset/Login.css";
+import GoogleBtn from "./GoogleBtn";
+import { landingAdminPath, rootPath, studentPath } from "./RoutePaths";
+import { connect } from "react-redux";
+import history from "./History";
+import Visibility from "@material-ui/icons/Visibility";
+import VisibilityOff from "@material-ui/icons/VisibilityOff";
+import IconButton from "@material-ui/core/IconButton";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import OutlinedInput from "@material-ui/core/OutlinedInput";
+import InputLabel from "@material-ui/core/InputLabel";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import FormControl from "@material-ui/core/FormControl";
+import { adminLogin } from "../Actions/AdminAction";
+import { isEmptyString } from "./Validation";
+import MySnackBar from "./MySnackBar";
+import DropDown from "./Controls/DropDown";
+import TextFieldComponent from "./Controls/TextField";
 export class Login extends Component {
   constructor(props) {
     super(props);
@@ -85,55 +87,73 @@ export class Login extends Component {
   };
 
   render() {
-    // 
-    // 
+    //
+    //
     return (
       <div>
-        <div className="root__login">
-          <div className="login__container">
-            <div className="login__left__container">
+        <div className='root__login'>
+          <div className='login__container'>
+            <div className='login__left__container'>
               <img
                 src={require("../Asset/Images/ProfileBuilder.png")}
-                alt="not suppotted"
+                alt='not suppotted'
               />
             </div>
-            <div className="login__right__container">
-              <div className="login__inner__box">
-                <div className="login__header">
-                  <label className="login__header__label">
+            <div className='login__right__container'>
+              <div className='login__inner__box'>
+                <div className='login__header'>
+                  <label className='login__header__label'>
                     Welcome to CareerLabs
                   </label>
                 </div>
-                <div className="login__body">
-                  <div className="error">
+                <div className='login__body'>
+                  <div className='error'>
                     {this.state.error.length !== 0 ? this.state.error : null}
                   </div>
-                  <div className="login__text__box">
+                  <div className='login__text__box'>
+                    <DropDown
+                      id={"combo-box-demo"}
+                      options={[]}
+                      // getOptionLabel={(option) => option.name}
+                      // value={eachItem.pgaAdditionalPoint}
+                      // onChange={(e, value) =>
+                      //   handleAdditionalPointChange(value, index)
+                      // }
+                      renderInput={(params) => (
+                        <TextFieldComponent
+                          {...params}
+                          label={"Role"}
+                          variant={"outlined"}
+                        />
+                      )}
+                    />
+                  </div>
+                  <div className='login__text__box'>
                     <TextField
-                      id="Username"
-                      label="Username"
-                      variant="outlined"
+                      id='Username'
+                      label='Username'
+                      variant='outlined'
                       helperText={this.state.userNameErr}
                       error={this.state.userNameErr.length > 0}
                       fullWidth
-                      size="medium"
+                      size='medium'
                       value={this.state.username}
                       onChange={(e) =>
                         this.setState({ username: e.target.value })
                       }
                     />
                   </div>
-                  <div className="login__text__box">
+                  <div className='login__text__box'>
                     <FormControl
                       error={this.state.passwordErr.length > 0}
-                      variant="outlined"
+                      variant='outlined'
                       fullWidth
                     >
-                      <InputLabel htmlFor="outlined-adornment-password">
+                      <InputLabel htmlFor='outlined-adornment-password'>
                         Password
                       </InputLabel>
                       <OutlinedInput
-                        id="outlined-adornment-password"
+                        id='outlined-adornment-password'
                         type={this.state.visibile ? "text" : "password"}
                         fullWidth
                         value={this.state.password}
@@ -147,15 +167,15 @@ export class Login extends Component {
                           }
                         }}
                         endAdornment={
-                          <InputAdornment position="end">
+                          <InputAdornment position='end'>
                             <IconButton
-                              aria-label="toggle password visibility"
+                              aria-label='toggle password visibility'
                               onClick={(e) =>
                                 this.setState({
                                   visibile: !this.state.visibile,
                                 })
                               }
-                              edge="end"
+                              edge='end'
                             >
                               {this.state.visibile ? (
                                 <Visibility />
@@ -171,12 +191,12 @@ export class Login extends Component {
                     </FormControl>
                   </div>
                 </div>
-                <div className="login__footer">
-                  <div className="login__button">
+                <div className='login__footer'>
+                  <div className='login__button'>
                     <Button
-                      id="login"
-                      variant="contained"
-                      color="primary"
+                      id='login'
+                      variant='contained'
+                      color='primary'
                       onClick={this.handleLogin.bind(this)}
                     >
                       Sign in
@@ -204,7 +224,7 @@ export class Login extends Component {
   }
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
     adminLoginDetails: state.AdminReducer.adminLoginDetails,
   };
