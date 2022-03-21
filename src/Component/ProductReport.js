aa29d3c65d80843271d28a41d088c4493bea8aab
balasuresh-icanio <balasuresh.p@icanio.com>
2022-03-21 21:18:05 +0530

    sales report changes

--- src/Component/ProductReport.js
+++ src/Component/ProductReport.js
@@ -1,69 +1,109 @@
-import { Breadcrumbs, Button, Grid, Typography } from "@material-ui/core";
+import {
+  Box,
+  Breadcrumbs,
+  Button,
+  Grid,
+  Table,
+  TableBody,
+  TableRow,
+  Typography,
+} from "@material-ui/core";
 import NavigateNextIcon from "@material-ui/icons/NavigateNext";
 import moment from "moment";
-import React, { useEffect, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import {
+  clearCustomData,
+  generateProductReport,
+  getProductReport,
+} from "../Actions/Reports";
 import BackButton from "../Asset/Images/backbutton.svg";
 import {
+  BlueCell,
+  BodyCell,
   BreadCrumpContainer,
+  Head,
+  HeadCell,
+  HeadInline,
   typographyStyle,
   useStyles,
 } from "../Asset/StyledComponents/ReportStyles";
+import PaginationComponent from "../Component/Utils/CustomPaginationComponent";
 import Loader from "../Lms/Utils/Loader";
 import TextFieldComponent from "./Controls/TextField";
-import { studentPath } from "./RoutePaths";
 import Snack from "./MySnackBar";
-import { clearCustomData, downloadProductReport } from "../Actions/Reports";
+import { studentPath } from "./RoutePaths";
 
 function ProductReport(props) {
   const classes = useStyles();
   const [state, setState] = useState({
-    isDownloading: false,
+    isGenerating: false,
     startDate: null,
     endDate: null,
     endDateHelperText: null,
     isDisabled: true,
     snackOpen: false,
     snackMsg: "",
+    productReportList: [],
+    page: 0,
+    totalPage: 0,
   });
   const {
     startDate,
     endDate,
     endDateHelperText,
     isDisabled,
-    isDownloading,
+    isGenerating,
     snackOpen,
     snackMsg,
+    page,
+    totalPage,
+    productReportList,
   } = state;
   const dispatch = useDispatch();
-  const { productReport } = useSelector(
+  const { productReport, generateReportStatus } = useSelector(
     (stateValue) => stateValue.ReportReducer
   );
 
   useEffect(() => {
-    if (productReport && isDownloading) {
+    if (generateReportStatus && isGenerating) {
+      setState({
+        ...state,
+        isGenerating: false,
+      });
+      dispatch(getProductReport(0, 10));
+      dispatch(clearCustomData("generateReportStatus"));
+    }
+  }, [generateReportStatus, isGenerating]);
+
+  useEffect(() => {
+    if (productReport) {
       if (productReport.success) {
-        const downloadUrl = window.URL.createObjectURL(
-          new Blob([productReport.data])
-        );
-        const link = document.createElement("a");
-        link.href = downloadUrl;
-        link.setAttribute("download", "Report.xls");
-        document.body.appendChild(link);
-        link.click();
-        link.remove();
-        setState({ ...state, isDownloading: false });
+        if (productReport.content && productReport.content.length !== 0) {
+          setState({
+            ...state,
+            productReportList: productReport.content || [],
+            totalPage: productReport.totalPages || 0,
+          });
+        } else {
+          setState({
+            ...state,
+            productReportList: [],
+            totalPage: 0,
+            snackOpen: true,
+            snackMsg: "No results found",
+          });
+        }
       } else {
         setState({
           ...state,
           snackOpen: true,
           snackMsg: productReport.message,
-          isDownloading: false,
         });
       }
       dispatch(clearCustomData("productReport"));
     }
-  }, [productReport, isDownloading]);
+  }, [productReport]);
 
   const compare = (dateTimeA, dateTimeB) => {
     if (dateTimeA && dateTimeB) {
@@ -95,19 +135,94 @@ function ProductReport(props) {
     setState({ ...state, [name]: value });
   };
 
-  const handleDownloadClick = () => {
+  const handleDownloadClick = (link) => {
+    if (link) {
+      window.location.href = link;
+    }
+  };
+
+  const handleGenerateClick = () => {
     setState({
       ...state,
-      isDownloading: true,
+      isGenerating: true,
+      page: 0,
+      totalPage: 0,
+      productReportList: [],
     });
-    dispatch(downloadProductReport(startDate, endDate));
+    dispatch(generateProductReport(startDate, endDate));
   };
 
   const handleSnackClose = () => {
     setState({ ...state, snackOpen: false, snackMsg: "" });
   };
 
-  return isDownloading ? (
+  const handlePageChange = (event, value) => {
+    setState({ ...state, page: value - 1 });
+    dispatch(getProductReport(value - 1, 10));
+  };
+
+  const renderTable = () => {
+    const columns = ["Created date", "Selection range", "Created by", ""];
+
+    return (
+      <Box overflow='auto' width='100%'>
+        <Fragment>
+          <Table>
+            <Head>
+              <TableRow>
+                {columns.map((item, index) => (
+                  <HeadCell key={index}>
+                    <HeadInline>{item}</HeadInline>
+                  </HeadCell>
+                ))}
+              </TableRow>
+            </Head>
+            <TableBody>
+              {productReportList &&
+                productReportList.length !== 0 &&
+                productReportList.map(
+                  (
+                    { createdTime, userSelectedDate, downloadLink, userRole },
+                    index
+                  ) => {
+                    return (
+                      <TableRow key={index} style={{ border: "0 0 0 0" }}>
+                        <BodyCell>
+                          {createdTime
+                            ? moment(new Date(createdTime)).format("DD-MM-YYYY")
+                            : "NA"}
+                        </BodyCell>
+                        <BodyCell>{userSelectedDate || "NA"}</BodyCell>
+                        <BlueCell>{userRole?.username || "NA"}</BlueCell>
+                        <BodyCell align={"right"}>
+                          <Button
+                            disabled={!Boolean(downloadLink)}
+                            color={"primary"}
+                            onClick={() => handleDownloadClick(downloadLink)}
+                            variant={"contained"}
+                          >
+                            {"Download"}
+                          </Button>
+                        </BodyCell>
+                      </TableRow>
+                    );
+                  }
+                )}
+            </TableBody>
+          </Table>
+        </Fragment>
+        {totalPage > 1 && (
+          <PaginationComponent
+            page={page + 1}
+            pageCount={totalPage}
+            onPageChange={handlePageChange}
+          />
+        )}
+      </Box>
+    );
+  };
+
+  return isGenerating ? (
     <Loader />
   ) : (
     <div>
@@ -181,13 +296,16 @@ function ProductReport(props) {
           container
         >
           <Button
-            disabled={isDownloading || isDisabled}
+            disabled={isGenerating || isDisabled}
             color={"primary"}
-            onClick={handleDownloadClick}
+            onClick={handleGenerateClick}
             variant={"contained"}
           >
-            {"Download"}
+            {"Generate"}
           </Button>
+        </Grid>
+        <Grid item xs={12}>
+          {productReportList && productReportList.length !== 0 && renderTable()}
         </Grid>
       </Grid>
       <Snack
