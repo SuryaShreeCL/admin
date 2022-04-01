ff6da7ebd3ce305e572b607a9fceaada82e23fe9
balasuresh-icanio <balasuresh.p@icanio.com>
2022-04-01 15:43:21 +0530

    test and servey url change

--- src/Actions/ScoreDetails.js
+++ src/Actions/ScoreDetails.js
@@ -1,25 +1,28 @@
-import { SCOREDETAILS} from '../Redux/Action'
-import {URL} from './URL'
-import axios from 'axios'
-export const viewscoredetails=(id,callback)=>{
-    let accessToken = window.sessionStorage.getItem("accessToken")
+import { SCOREDETAILS } from "../Redux/Action";
+import { URL } from "./URL";
+import axios from "axios";
+export const viewscoredetails = (studentId, callback) => {
+  let accessToken = window.sessionStorage.getItem("accessToken");
 
-    return dispatch => {
-        axios.get(URL+"/api/v1/students/score/"+id, {
-            crossDomain: true,
-            headers : {
-                "admin" : "yes",
-                "Authorization" : `Bearer ${accessToken}`
-            }
-            
-        })
-            .then(result => {
-                callback(result)
-               dispatch({type:SCOREDETAILS.viewScoreDetails,viewScoreDetailsList:result.data})
-            })
-            .catch(error => {
-                // callback(error)
-                console.log(error);
-            });
-    }
-}
+  return (dispatch) => {
+    axios
+      .get(`${URL}/api/v1/students/score/${studentId}`, {
+        crossDomain: true,
+        headers: {
+          admin: "yes",
+          Authorization: `Bearer ${accessToken}`,
+        },
+      })
+      .then((result) => {
+        callback(result);
+        dispatch({
+          type: SCOREDETAILS.viewScoreDetails,
+          viewScoreDetailsList: result.data,
+        });
+      })
+      .catch((error) => {
+        // callback(error)
+        console.log(error);
+      });
+  };
+};
