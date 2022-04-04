274f246b7a4255b274e71668f5c7792f81ddd64c
Dhinakaran-C <99329196+Dhinakaran-C@users.noreply.github.com>
2022-04-04 17:41:17 +0530

    pushing action,reducer and redux code for strategy session

--- src/Actions/StrategySession.js
+++ src/Actions/StrategySession.js
@@ -2,52 +2,15 @@ import axios from "axios";
 import { URL } from "./URL";
 import { STRATEGY_SESSION } from "../Redux/Action";
 
-export const putProgramPrefernce = (data, studentId, productId) => {
-  return (dispatch) => {
-    let accessToken = window.sessionStorage.getItem("accessToken");
 
-    axios
-      .put(
-        URL +
-          "/api/v1/students/" +
-          studentId +
-          "/products/" +
-          productId +
-          "/strategySession/programPreference",
-        data,
-        {
-          crossDomain: true,
-          headers: {
-            admin: "yes",
-            Authorization: `Bearer ${accessToken}`,
-          },
-        }
-      )
-      .then((result) => {
-        console.log(result);
-        dispatch({
-          type: STRATEGY_SESSION.putProgramPrefernce,
-          putProgramPrefernce: result.data,
-        });
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-};
 
-export const getProgramPrefence = (studentId, productId) => {
+export const getStageId = () => {
   return (dispatch) => {
     let accessToken = window.sessionStorage.getItem("accessToken");
 
     axios
       .get(
-        URL +
-          "/students/" +
-          studentId +
-          "/products/" +
-          productId +
-          "/strategySession/programPreference",
+        `${URL}/api/v1/students/9fd9b445-6237-4fe0-b9af-2c6aa17a50e2/products/4/stages`,
         {
           headers: {
             admin: "yes",
@@ -57,7 +20,7 @@ export const getProgramPrefence = (studentId, productId) => {
       )
       .then((result) => {
         dispatch({
-          type: STRATEGY_SESSION.getProgramPreference,
+          type: STRATEGY_SESSION.getStageId,
           payload: result.data,
         });
       })
@@ -66,3 +29,107 @@ export const getProgramPrefence = (studentId, productId) => {
       });
   };
 };
+
+export const getTemplateUsingStageId = (studentId, productId,subStagesId) => {
+  return (dispatch) => {
+    let accessToken = window.sessionStorage.getItem("accessToken");
+
+    axios
+      .get(`${URL}api/v1/students/${studentId}/products/${productId}/subStages/${subStagesId}`,
+        {
+          headers: {
+            admin: "yes",
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      )
+      .then((result) => {
+        dispatch({
+          type: STRATEGY_SESSION.getTemplateUsingStageId,
+          payload: result.data,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+};
+
+export const postStudentDocumentUsingStageId = (studentId, productId,subStagesId) => {
+  return (dispatch) => {
+    let accessToken = window.sessionStorage.getItem("accessToken");
+
+    axios
+      .post(
+        `${URL}api/v1/students/${studentId}/products/${productId}/subStages/${subStagesId}/fileUpload`,
+        {
+          headers: {
+            admin: "yes",
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      )
+      .then((result) => {
+        dispatch({
+          type: STRATEGY_SESSION.postStudentDocumentUsingStageId,
+          payload: result.data,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+};
+
+export const putSaveFileDetailsUsingStageId = (studentId, productId,subStagesId) => {
+  return (dispatch) => {
+    let accessToken = window.sessionStorage.getItem("accessToken");
+
+    axios
+      .put(
+        `${URL}api/v1/students/${studentId}/products/${productId}/subStages/${subStagesId}`,
+        {
+          headers: {
+            admin: "yes",
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      )
+      .then((result) => {
+        dispatch({
+          type: STRATEGY_SESSION.putSaveFileDetailsUsingStageId,
+          payload: result.data,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+};
+
+export const getDownloadFileUsingStageId = (studentId,subStagesId) => {
+  return (dispatch) => {
+    let accessToken = window.sessionStorage.getItem("accessToken");
+
+    axios
+      .get(
+        `${URL}/students/${studentId}/subStage/${subStagesId}/{fileName}`,
+        {
+          headers: {
+            admin: "yes",
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      )
+      .then((result) => {
+        dispatch({
+          type: STRATEGY_SESSION.getDownloadFIleUsingStageId,
+          payload: result.data,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+};
+
