500c37e9f257f8363f08175f46f6c02880a681a1
vishvj-2507 <vijayeswarlal1999@gmail.com>
2022-06-18 16:14:58 +0530

    Admin - test validation removal

--- src/Component/Test/Pages/CreateTest.jsx
+++ src/Component/Test/Pages/CreateTest.jsx
@@ -129,15 +129,6 @@ const CreateTest = () => {
   const [testCreated, setTestCreated] = useState(false);
 
   const validate = (values) => {
-    if (values.wallFiles.length === 0) {
-      setNotify({
-        isOpen: true,
-        message: 'Please upload image(s)',
-        type: 'error',
-      });
-      return false;
-    }
-
     //validation of event link
     if (values?.eventPost?.id?.length < 5) {
       setNotify({
