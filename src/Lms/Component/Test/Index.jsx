7ae27d510d19a60c8ead4eebf38333120984f6fe
vishvj-2507 <vijayeswarlal1999@gmail.com>
2022-06-24 17:49:00 +0530

    Reschedule popup timing fixes

--- src/Lms/Component/Test/Index.jsx
+++ src/Lms/Component/Test/Index.jsx
@@ -102,7 +102,6 @@ class TestLanding extends Component {
       alertSeverity: "",
       popupOpen: false,
       popupOpen1: false,
-      eventDate: "",
       eventDate: new Date(),
       eventEndDate: new Date(),
       openStatus: false,
@@ -324,9 +323,10 @@ class TestLanding extends Component {
       const { data: tableContent } = this.props.testData;
 
       if (tableContent) {
-        let findObj = tableContent.content.map(
+        let findObj = tableContent.content.filter(
           (el) => el.id === this.state.popUpId
-        );
+        )[0];
+        console.log(tableContent,findObj,"findObj.eventDate")
 
         if (findObj) {
           this.setState({
