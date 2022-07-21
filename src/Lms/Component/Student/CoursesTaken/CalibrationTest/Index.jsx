70d82d7e9083b497e464d8e34b7f2387ba8daf9d
balasuresh-icanio <balasuresh.p@icanio.com>
2022-07-21 18:38:33 +0530

    calibration test finish

--- src/Lms/Component/Student/CoursesTaken/CalibrationTest/Index.jsx
+++ src/Lms/Component/Student/CoursesTaken/CalibrationTest/Index.jsx
@@ -1,31 +1,20 @@
-import { Box, Grid, Typography } from "@material-ui/core";
+import { Box, Grid } from "@material-ui/core";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import {
-  FlexColumnView,
-  FlexView,
-  InsightSubTitle,
-  LevelContent,
-  SideContent,
-} from "../../../../Assets/StyledComponents";
-import { SnackBar } from "../../../../Utils/SnackBar";
-import React from "react";
+import { CenterText } from "../../../../Assets/StyledComponents";
 import { getCalibrationTestReport } from "../../../../Redux/Action/Student";
-import { customDateFormat } from "../../../../Utils/HelperFunction";
-import { useStyles } from "./Style";
-
-const BOX_COLOR = "rgba(254, 187, 44, 0.5)";
+import { SnackBar } from "../../../../Utils/SnackBar";
+import OverAllScoreCard from "./OverAllScoreCard";
+import SubjectCard from "./SubjectCard";
+import React from "react";
 
 function CalibrationTest({ studentId, courseId }) {
-  const classes = useStyles();
   const dispatch = useDispatch();
   const { calibrationTestReport } = useSelector(
     (state) => state.LmsStudentReducer
   );
   const [state, setState] = useState({
     insights: null,
-    review: [],
-    page: 0,
   });
 
   const [snack, setSnack] = useState({
@@ -34,7 +23,7 @@ function CalibrationTest({ studentId, courseId }) {
     color: "",
   });
 
-  const { insights, review, page } = state;
+  const { insights } = state;
 
   const { open, message, color } = snack;
 
@@ -54,14 +43,16 @@ function CalibrationTest({ studentId, courseId }) {
 
   useEffect(() => {
     if (calibrationTestReport) {
-      if (!calibrationTestReport.success) {
-        console.log(calibrationTestReport);
+      if (calibrationTestReport.success) {
         setState({
           ...state,
           insights: calibrationTestReport.data?.insights,
-          review: calibrationTestReport.data?.review || [],
         });
       } else {
+        setState({
+          ...state,
+          insights: null,
+        });
         setSnack({
           open: true,
           message: calibrationTestReport.message,
@@ -71,119 +62,21 @@ function CalibrationTest({ studentId, courseId }) {
     }
   }, [calibrationTestReport]);
 
-  const handlePageChange = (newPage) => {
-    if (page !== newPage)
-      setState({
-        ...state,
-        page: newPage,
-      });
-  };
-
   return (
     <Box padding={"15px 0px !important"}>
       <Grid container spacing={2}>
-        {insights && (
+        {insights ? (
           <>
-            <Grid item xs={12}>
-              <Box>
-                <FlexView gap={"30px"}>
-                  <Box>
-                    <FlexView gap={"8px"}>
-                      <InsightSubTitle>
-                        {insights?.donut?.overall?.title}
-                      </InsightSubTitle>
-                      <InsightSubTitle>{`${insights?.donut?.overall?.score}/${insights?.donut?.overall?.total}`}</InsightSubTitle>
-                    </FlexView>
-                  </Box>
-                  <Box>
-                    <FlexView gap={"50px"}>
-                      <Box>
-                        <FlexView gap={"16px"}>
-                          <Typography
-                            variant={"subtitle1"}
-                            style={{ fontWeight: 600 }}
-                          >
-                            {"Start Date:"}
-                          </Typography>
-                          <Typography variant={"subtitle1"}>
-                            {customDateFormat(
-                              insights?.reportDate,
-                              "DD MMM YYYY"
-                            ) || "NA"}
-                          </Typography>
-                        </FlexView>
-                      </Box>
-                      <Box>
-                        <FlexView gap={"16px"}>
-                          <Typography
-                            variant={"subtitle1"}
-                            style={{ fontWeight: 600 }}
-                          >
-                            {"End Date:"}
-                          </Typography>
-                          <Typography variant={"subtitle1"}>
-                            {customDateFormat(
-                              insights?.startDate,
-                              "DD MMM YYYY"
-                            ) || "NA"}
-                          </Typography>
-                        </FlexView>
-                      </Box>
-                    </FlexView>
-                  </Box>
-                </FlexView>
-              </Box>
-            </Grid>
-            {insights.donut.total.length !== 0 &&
-              insights.donut.total.map((item, index) => (
-                <Grid item xs={3}>
-                  <Box bgcolor={BOX_COLOR} padding={"24px !important"}>
-                    <FlexView>
-                      <Typography
-                        variant={"subtitle1"}
-                        className={classes.subText}
-                      >
-                        {item.label}
-                      </Typography>
-                      <Typography variant={"h5"} className={classes.subText}>
-                        {item.total
-                          ? `${insights.donut.score[index]}/${item.total}`
-                          : insights.donut.score[index]}
-                      </Typography>
-                    </FlexView>
-                  </Box>
-                </Grid>
-              ))}
-            {insights.pie.card.length !== 0 &&
-              insights.pie.card.map(({ title, events }, index) => (
-                <Grid item xs={4}>
-                  <Box
-                    bgcolor={BOX_COLOR}
-                    padding={"20px 16px !important"}
-                    minHeight={"100%"}
-                  >
-                    <FlexColumnView gap={"20px"}>
-                      <LevelContent>{title}</LevelContent>
-                      {events &&
-                        events.length !== 0 &&
-                        events.map((item) => (
-                          <FlexView>
-                            <SideContent>{item.title}</SideContent>
-                            <Typography
-                              variant={"subtitle1"}
-                              className={classes.subText}
-                            >
-                              {`${item.time} mins`}
-                            </Typography>
-                          </FlexView>
-                        ))}
-                    </FlexColumnView>
-                  </Box>
-                </Grid>
-              ))}
+            <OverAllScoreCard insights={insights} />
+            <SubjectCard subjects={insights?.subjects} />
           </>
+        ) : (
+          <Grid item xs={12}>
+            <CenterText paddingTop={"200px !important"}>
+              {"Calibration Test not yet Initiated"}
+            </CenterText>
+          </Grid>
         )}
-        {review.length !== 0 && <Grid item xs={12}></Grid>}
       </Grid>
       <SnackBar
         snackData={{
